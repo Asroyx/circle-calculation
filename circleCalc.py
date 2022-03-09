6a07b9787cb55fc7494455d4bea761ba9108caf2
Asil Ayazoglu <90615063+Asroyx@users.noreply.github.com>
2022-03-09 14:00:55 +0300

    Add files via upload

---
+++ circleCalc.py
@@ -0,0 +1,12 @@
+'''
+    Circle area : πr2
+    Circle circumference  : 2πr2
+
+    Calculate the area and perimeter of a circle given the radius
+'''
+
+r = float(input("Enter the radius of the circle : "))
+cArea= 3.14*(r**2)
+cCircumference = 2*3.14*r
+print("Circle area :",cArea,"\nCircle circumference :",cCircumference)
+input()
